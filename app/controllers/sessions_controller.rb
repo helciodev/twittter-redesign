7e707a3a100e744d6d2d21c3e81f36ebc3f0c529
helciodev <helcioandre@live.com>
2021-03-30 13:30:07 +0100

    Add create and destroy actions for user sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  def create
+    user = User.find_by_username(params[:username])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path flash[:success] = 'You have succesfully logged out'
+  end
 end
