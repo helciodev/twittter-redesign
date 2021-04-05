489f9546c50e735584ee7d8add584c034cdab194
helciodev <helcioandre@live.com>
2021-04-05 11:52:52 +0100

    Add params for users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :profile_photo, :cover_photo)
   end
 end
