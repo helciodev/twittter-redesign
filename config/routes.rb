cdafde540aa5012f43800da6c32437ae10b223e6
helciodev <helcioandre@live.com>
2021-04-05 11:47:42 +0100

    Add login and logout session to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'opinions#new'
   resources :opinions
   resources :users
-  resources :sessions, only: [:new, :create,:destroy]
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
