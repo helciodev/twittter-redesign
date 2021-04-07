055db104489cdbb209f22373a120a6f70738a808
helciodev <helcioandre@live.com>
2021-04-07 15:03:25 +0100

    Add sessions/new view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:session][:name].downcase)
     if user
       log_in user
-      redirect_back_or user
+      redirect_back_or root_url
     else
       flash.now[:danger] = 'Invalid name'
       render 'new'
