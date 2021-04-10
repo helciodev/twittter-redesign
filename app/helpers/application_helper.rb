d0e37155425e8cfe0664d56f90b6dc86104e22d4
helciodev <helcioandre@live.com>
2021-04-10 12:04:42 +0100

    Create helper method for messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def user_profile_photo(user)
     user.profile_photo.attached? ? user.profile_photo : 'default-profile'
   end
+
+  def message
+    if flash[:notice]
+      content_tag(:div, content_tag(:span, flash[:notice]), class: 'alert-success')
+    elsif flash[:alert]
+      content_tag(:div, content_tag(:span, flash[:alert]), class: 'alert-danger')
+    end
+  end
 end
