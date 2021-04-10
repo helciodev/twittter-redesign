5221b006359523314896c947484bb10940aaa40e
helciodev <helcioandre@live.com>
2021-04-10 17:53:15 +0100

    Comment out tests produced automatically by rails

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  fixtures :users
+
+  it 'returns true for a valid user' do
+    user1 = users(:user1)
+    expect(user1.name.valid?).to be true
+  end
+
+  it 'returns false for an name is more than 20 characters' do
+    user3 = users(:user3)
+    expect(user3.name.valid?).to be false
+  end
 end
+
+# describe User, type: :model do
+#   fixtures :users
+
+#   it 'returns true for a valid user' do
+#     user1 = users(:user1)
+#     expect(user1.valid?).to be true
+#   end
+
+#   it 'returns false for an name is more than 20 characters' do
+#     user3 = users(:user3)
+#     expect(user3.valid?).to be false
+#   end
+
+#   it 'invalid if email already exit' do
+#     user = User.new(name: 'wand', email: 'wandji@email.com')
+#     expect(user.valid?).to be false
+#   end
+
+#   describe 'columns for user' do
+#     it { should have_db_column(:email) }
+#     it { should have_db_column(:name) }
+#   end
+# end
