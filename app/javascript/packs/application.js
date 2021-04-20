4e03daf66f2676823865d45a872b88346f28a2b5
helciodev <helcioandre@live.com>
2021-04-20 16:22:01 +0100

    Include //= cloudinary in application.js change image_tag helper for cl_image_tag and configure cloudinary.yml file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,6 +3,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require activestorage
+
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
