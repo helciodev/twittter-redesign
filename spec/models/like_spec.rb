6313d88d2ccbd70c9b079fd385599c506e6c0316
helciodev <helcioandre@live.com>
2021-04-29 16:30:05 +0100

    Add test for like model

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  context 'Association' do
+    it { should belong_to(:user) }
+    it { should belong_to(:opinion) }
+  end
 end
+
