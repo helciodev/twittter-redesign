445caeb4f54f7a5f5c3b0adb3d8dbadd31abb3dd
helciodev <helcioandre@live.com>
2021-04-30 14:58:52 +0100

    Delete migration rename_cover_photo_to_cover_photo_data_in_users

--- db/migrate/20210427151522_rename_cover_photo_to_cover_photo_data_in_users.rb
+++
@@ -1,9 +0,0 @@
-class RenameCoverPhotoToCoverPhotoDataInUsers < ActiveRecord::Migration[6.1]
-  def up
-    rename_column :users, :cover_photo_data, :cover_photo
-  end
-
-  def def down 
-    rename_column :users, :cover_photo, :cover_photo_data
-  end
-end
